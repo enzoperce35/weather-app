40c790721223ae4d862f38ef7ce1fc6072df64a3
enzoperce35 <enzoperce35@gmail.com>
2023-10-19 14:54:06 +0800

    live preview test

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import { displayWeather } from './async_function/promise_then';
 import './index.css';
 
 // A classic promise_then async func; newer async functions are called on form submissions.
-displayWeather(storage.defaultCity)
-  .then((data) => {
-    PubSub.publish('data_ready', data);
-  },
-
-  (error) => {
-    alert(error);
-});
+//displayWeather(storage.defaultCity)
+//  .then((data) => {
+//    PubSub.publish('data_ready', data);
+//  },
+//
+//  (error) => {
+//    alert(error);
+//});
+console.log(storage.defaultCity)
